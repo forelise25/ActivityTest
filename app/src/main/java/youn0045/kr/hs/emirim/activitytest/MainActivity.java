b63bc9845e8578986dda40e2f7f694f63331f5a0
forelise25 <forelise25@naver.com>
2017-04-13 20:58:39 +0900

    #5 ActivityTest5

--- app/src/main/java/youn0045/kr/hs/emirim/activitytest/MainActivity.java
+++ app/src/main/java/youn0045/kr/hs/emirim/activitytest/MainActivity.java
@@ -4,20 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editName, editPhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button butmain=(Button)findViewById(R.id.but_main);
         butmain.setOnClickListener(butMainHandler);
+        editName=(EditText)findViewById(R.id.edit_name);
+        editName=(EditText)findViewById(R.id.edit_phone);
     }
     View.OnClickListener butMainHandler=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            String name = editName.getText().toString();
+            String phone = editPhone.getText().toString();
+            intent.putExtra("name", name);
+            intent.putExtra("phone",phone);
             startActivity(intent);
         }
     };
--- app/src/main/java/youn0045/kr/hs/emirim/activitytest/SecondActivity.java
+++ app/src/main/java/youn0045/kr/hs/emirim/activitytest/SecondActivity.java
@@ -1,11 +1,14 @@
 package youn0045.kr.hs.emirim.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    String name, phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,11 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         Button butBack = (Button)findViewById(R.id.but_second);
         butBack.setOnClickListener(butBackHandler);
+        Intent intent = getIntent();
+        name=intent.getStringExtra("name");
+        phone=intent.getStringExtra("phone");
+        TextView textResult = (TextView)findViewById(R.id.text_result);
+        textResult.setText("## 성명 : "+name+"\n"+"## 전화번호 : "+phone);
     }
 
     View.OnClickListener butBackHandler=new View.OnClickListener(){
