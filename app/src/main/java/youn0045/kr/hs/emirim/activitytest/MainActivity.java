121132e3a285bf6ca42c90f8fd2992a0c307f6bb
forelise25 <forelise25@naver.com>
2017-04-13 20:21:37 +0900

    #3 JavaEventProgramming

--- app/src/main/java/youn0045/kr/hs/emirim/activitytest/MainActivity.java
+++ app/src/main/java/youn0045/kr/hs/emirim/activitytest/MainActivity.java
@@ -1,12 +1,24 @@
 package youn0045.kr.hs.emirim.activitytest;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button butmain=(Button)findViewById(R.id.but_main);
+        butmain.setOnClickListener(butMainHandler);
     }
+    View.OnClickListener butMainHandler=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+    };
 }
